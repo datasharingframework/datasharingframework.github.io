edace74dc2ce22a660fe9c194b317e737c6e9bb6
Simon Schweizer <simon@schwzr.de>
2025-11-25 23:10:54 +0100

    set 2.0.0 to latest version, adopted menu

--- docs/src/.vuepress/layouts/PageLayout.vue
+++ docs/src/.vuepress/layouts/PageLayout.vue
@@ -4,7 +4,7 @@ import { useRoute, useRouter } from "vue-router";
 import { ref, onMounted } from 'vue'
 
 const version = ref("");
-const latestVersion = "v1.9.0";
+const latestVersion = "v2.0.0";
 
 
 function setVersionBasedOnCurrentPath() : void {
@@ -55,8 +55,8 @@ function navigateToNewVersion() {
       <div class="version-selector" v-if="route.path.startsWith('/operations/')">
         <label class="vp-sidebar-header" for="version-select"><strong>Version:</strong> </label>
         <select id="version-select" class="vp-sidebar-header" v-model="version" @change="navigateToNewVersion">
-        <option value="v2.0.0-RC2">2.0.0-RC2</option>
-        <option value="v1.9.0">latest (1.9.0)</option>
+        <option value="v2.0.0">latest (2.0.0)</option>
+        <option value="v1.9.0">1.9.0</option>
         <option value="v1.8.0">1.8.0</option>
         <option value="v1.7.1">1.7.1</option>
         <option value="v1.7.0">1.7.0</option>
--- docs/src/.vuepress/theme.ts
+++ docs/src/.vuepress/theme.ts
@@ -39,12 +39,12 @@ export default hopeTheme({
           icon: "launch",
           prefix: "/operations/",
           children: [{
-            text: "Current version - 1.9.0",
-            link: "get-started.md",
+            text: "Stable version - 1.9.0",
+            link: "v1.9.0/readme.md",
             icon: "launch"
           }, {
-            text: "Next version - 2.0.0-RC2",
-            link: "v2.0.0-RC2/",
+            text: "Current version - 2.0.0",
+            link: "get-started.md",
             icon: "launch"
           }, "old-versions.md"],
         },
@@ -128,9 +128,9 @@ export default hopeTheme({
     "/spring-school": [],
     "/news": [],
     "/operations/old-versions": [],
-    "/operations/latest/": generate_v1_latest_sidebar(),
+    "/operations/latest/": generate_v2_latest_sidebar(),
     "/operations/next/": [],
-    "/operations/v2.0.0-RC2/": generate_v2_latest_sidebar(),
+    "/operations/v2.0.0/": generate_v2_latest_sidebar(),
     "/operations/v1.9.0/": generate_v1_latest_sidebar(),
     "/operations/v1.8.0/": generate_v1_gt_eq_1_7_0_sidebar(),
     "/operations/v1.7.1/": generate_v1_gt_eq_1_7_0_sidebar(),
