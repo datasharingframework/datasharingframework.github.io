13c9265d4eca035b151d1f178f566da767eb7335
Serap Aydin <saydin3@stud.hs-heilbronn.de>
2026-02-18 18:06:05 +0100

    newslist removed bc its not used

--- docs/src/.vuepress/client.ts
+++ docs/src/.vuepress/client.ts
@@ -4,18 +4,17 @@ import { Layout as ParentLayout } from "vuepress-theme-hope/client";
 import Layout from './layouts/PageLayout.vue'
 import NotFoundLayout from './layouts/NotFoundLayout.vue'
 import BlogLayout from './layouts/BlogLayout.vue';
-import NewsList from "./components/NewsList.vue"; // adjust if path differs
+
 
 
 export default defineClientConfig({
     enhance({ app }) {
-    //app.component("NewsList", NewsList);
+    
   },
   layouts: {
     ParentLayout,
     Layout,
     NotFound: NotFoundLayout,
     Blog: BlogLayout,
-  
   },
 })
