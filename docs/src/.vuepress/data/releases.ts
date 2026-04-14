07ff3903e9ac1829f2a5e083d773b1e643851206
Simon Schweizer <simon@schwzr.de>
2026-04-14 12:05:14 +0200

    added digests

--- docs/src/.vuepress/data/releases.ts
+++ docs/src/.vuepress/data/releases.ts
@@ -18,10 +18,10 @@ export const releases: Record<string, Release> = {
     tag: '2.1.0',
     previousTag: '2.0.2',
     images: {
-      fhir:       { digest: 'sha256:TODO' },
-      fhir_proxy: { digest: 'sha256:TODO' },
-      bpe:        { digest: 'sha256:2087a12f2cc8386a019bc9706f8bcfdcd5ee9e12da07ca2bd5f09aaaba8133d7' },
-      bpe_proxy:  { digest: 'sha256:TODO' },
+      fhir:       { digest: 'sha256:71599af143f0262a7265aa2bc4ea5a9660f11de6248a053e060b5667070203fd' },
+      fhir_proxy: { digest: 'sha256:9f11a3580c970314532f5951808be6fe72f1de7d53348e625d2dd0c95bcf1d96' },
+      bpe:        { digest: 'sha256:3ee7ef0ac201fc3776273fbfc2569bdc4edf724a2bb9f1b4a889eb7e13ff4049' },
+      bpe_proxy:  { digest: 'sha256:c67da4a1720ea75a383764db2bf25619fe70f57773b1069029f5b49588eb1ecc' },
     },
   },
 };
